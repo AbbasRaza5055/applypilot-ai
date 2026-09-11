87b1f067098894f8bfd8e9e3017852d491b66d73
Abbas Raza <abbasraza5055@gmail.com>
2026-09-11 11:06:57 +0500

    Add imports and __all__ to models package

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,13 @@
+from .profile import CandidateProfile
+from .opportunity import Opportunity
+from .matching import MatchResult
+from .ats import ATSReport
+from .application import ApplicationPackage
 
+__all__ = [
+    "CandidateProfile",
+    "Opportunity",
+    "MatchResult",
+    "ATSReport",
+    "ApplicationPackage",
+]
